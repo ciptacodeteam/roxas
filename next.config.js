7275f663b4a4c9305895adae346117dc7dafde88
RaynaldoHuang <huangraynaldo@gmail.com>
2025-11-24 03:25:56 +0700

    error fix

--- next.config.js
+++ next.config.js
@@ -1,24 +1,8 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
-// A. Import validasi environment Anda (dari blok 1)
-import "./src/env.js"; 
-
-// B. Import plugin next-intl (dari blok 2)
+import './src/env.js';
 import createNextIntlPlugin from 'next-intl/plugin';
 
-// Baris "import type" telah dihapus karena ini adalah file JS.
-
-/** @type {import("next").NextConfig} */
-// Anotasi tipe NextConfig menggunakan JSDoc (tetap memberikan type checking di editor)
-const nextConfig = {
-  // Masukkan konfigurasi Next.js dasar Anda di sini (misalnya, images, webpack, dll.)
-  // Saat ini masih kosong, seperti pada blok 1 dan blok 2.
-};
-
-// C. Bungkus (wrap) konfigurasi dasar Anda dengan plugin next-intl
 const withNextIntl = createNextIntlPlugin();
 
-// D. Export hasil pembungkusan (dari blok 2)
+const nextConfig = {};
+
 export default withNextIntl(nextConfig);
